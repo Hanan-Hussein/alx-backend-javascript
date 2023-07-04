c1394594e12a5b7e9ac9c4c82918275f1fbb8580
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 12:43:52 +0000

    ft:correction-added corrections

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -11,7 +11,9 @@
 export default function getStudentIdsSum(students) {
    if (students instanceof Array) {
       return students.reduce(
-         (accumulator, currentValue) => accumulator.id || accumulator + currentValue.id, 0);
+         (accumulator, currentValue) => accumulator.id || accumulator + currentValue.id, 
+         0,
+      );
    }
    return 0
 }
