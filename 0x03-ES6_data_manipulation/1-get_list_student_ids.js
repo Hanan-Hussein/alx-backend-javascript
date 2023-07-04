15349c71b9ca82e676273ea94c5aeef4b6dd6a41
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 15:25:21 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,23 +1,16 @@
 /**
-* Retrieves ids from a list of students.
-* @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @author Hanan Hussein <https://github.com/Hanan-Hussein>
-* @returns
-*/
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Hanan Hussein <https://github.com/Hanan-Hussein>
+ * @returns
+ */
 export default function getListStudentIds(students) {
-  let allId = []
-  
-  if (Array.isArray(students)) {
-    students.map((std) => {
-	allId.push(std.id)
-	})
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
   }
-  else {
-    allId = []
-  }
-  return allId
+  return [];
 }
