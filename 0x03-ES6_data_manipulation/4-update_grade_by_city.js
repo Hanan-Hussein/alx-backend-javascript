1e91931779f2b1b9f361c6beec11b9093c8999f2
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 16:00:39 +0300

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,35 +1,32 @@
 /**
-* Updates the grades of a list of students in a given city.
-* @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} students - The list of students.
-* @param {*} city - The city of students.
-* @param {{
-*   studentId: Number,
-*   grade: Number,
-* }[]} newGrades - The new grades to be given to a student.
-* @author Hanan Hussein <https://github.com/Hanan-Hussein>
-* @returns {{id: Number, firstName: String, location: String}[]}
-*/
+ * Updates the grades of a list of students in a given city.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {*} city - The city of students.
+ * @param {{
+ *   studentId: Number,
+ *   grade: Number,
+ * }[]} newGrades - The new grades to be given to a student.
+ * @author Hanan Hussein <https://github.com/Hanan-Hussein>
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  let updatedStd = [];
+  const defaultGrade = { grade: 'N/A' };
 
   if (students instanceof Array) {
-    students.filter((std) => {
-        if (std.location === city) {
-            newGrades.map((grade) => {
-                if (grade.studentId === std.id) {
-                    std.grade = grade.grade;
-                }
-                else {
-                    std.grade == null ? std.grade = 'N/A' : std.grade;
-                }
-            })
-            updatedStd.push(std);
-        }
-    })
-}
-return updatedStd;
+    return students
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
+  }
+  return [];
 }
