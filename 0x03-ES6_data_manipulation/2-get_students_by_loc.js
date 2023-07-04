2600a09d41e533b22e3dea27fa890d763c9ab495
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 12:08:14 +0000

    ft:correction-added corrections

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,14 @@
-export function getStudentsByLocation(students, loct) {
-
-    return students.filter(std => loct == std.location)
-
+/**
+* Retrieves students in a given location.
+* @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @param {String} loct - The location.
+* @author Hanan Hussein <https://github.com/Hanan-Hussein>
+* @returns
+*/
+export default function getStudentsByLocation(students, loct) {
+  return students.filter(std => loct == std.location)
 }
--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,13 @@
-export function getStudentIdsSum(students) {
-
-   return students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0)
+/**
+ * Retrieves the sum of ids of a list of students.
+ * @param {{
+*   id: Number,
+*   firstName: String,
+*   location: String
+* }[]} students - The list of students.
+* @author Hanan Hussein <https://github.com/Hanan-Hussein>
+* @returns {Number}
+*/
+export default function getStudentIdsSum(students) {
+  return students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0)
 }
