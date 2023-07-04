7e99608cc198342a304daa0668bd550a81d240c7
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 11:38:19 +0000

    refactor:comments-added comments

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,12 +4,9 @@
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export function getListStudents() {
-
-    let students = [
+    return [
         { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
         { id: 2, firstName: 'James', location: 'Columbia' },
         { id: 5, firstName: 'Serena', location: 'San Francisco' }
     ];
-	return students
-
 }
