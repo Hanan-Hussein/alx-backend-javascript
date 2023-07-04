e55b2de4757dd60a0a65761bacb414b08cad02b1
Hanan Hussein <hanhuss035@gmail.com>
2023-07-04 12:37:14 +0000

    ft:correction-added corrections

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -6,11 +6,12 @@
 *   location: String
 * }[]} students - The list of students.
 * @author Hanan Hussein <https://github.com/Hanan-Hussein>
-* @returns {Number}
+* @returns {Number}  
 */
 export default function getStudentIdsSum(students) {
    if (students instanceof Array) {
-      return students.reduce((accumulator, currentValue) => accumulator + currentValue.id, 0)
+      return students.reduce(
+         (accumulator, currentValue) => accumulator.id || accumulator + currentValue.id, 0);
    }
    return 0
 }
