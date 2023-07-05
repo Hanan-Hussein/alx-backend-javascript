1119ad45b81205175e74f739183f1ee71e6c574c
Hanan Hussein <hanhuss035@gmail.com>
2023-07-05 20:56:27 +0000

    sets- check values in a set

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,10 @@
+/**
+ * Creates a set from an array.
+ * @param {Set} set - The collection of unique array.
+ * @param {*} array - The array of items.
+ * @author Hanan Hussein <https://github.com/Hanan-Hussein>
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+    return array.every((value) => set.has(value));
+  }
