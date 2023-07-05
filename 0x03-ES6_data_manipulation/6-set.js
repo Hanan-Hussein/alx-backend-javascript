893a9b41fc12832fd9caf57b3297a6d125c38869
Hanan Hussein <hanhuss035@gmail.com>
2023-07-05 21:06:53 +0000

    sets- check values in a set

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,7 +1,7 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Hanan Hussein <https://github.com/Hanan-Hussein>
  * @returns {Set}
  */
 export default function setFromArray(array) {
