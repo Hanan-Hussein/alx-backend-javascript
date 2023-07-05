5855bb30577aed3df0f1660fba4533c21b30a22d
Hanan Hussein <hanhuss035@gmail.com>
2023-07-05 23:14:31 +0300

    Update 6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,9 +1,9 @@
 /**
  * Creates a set from an array.
  * @param {Array} array - The source array.
- * @author Hanan Hussein <https://github.com/Hanan-Hussein>
+ * @author Bezaleel Olakunori <https://github.com/B3zaleel>
  * @returns {Set}
  */
 export default function setFromArray(array) {
-    return new Set(array);
-  }
+  return new Set(array);
+}
